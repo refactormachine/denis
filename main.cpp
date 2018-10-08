09c3cb03e794ddfa9a0c0c754834dfe78ee4f428
Shalti <talshalti1@gmail.com>
2018-10-08 11:44:13 +0300

    moved to main, reduced indentation

--- main.cpp
+++ main.cpp
@@ -1,34 +1,46 @@
-// Get all files in /data
-for (recursive_directory_iterator y(data), end; y != end; ++y) {
-// If path is not a directory
-if (!is_directory(y->path()))
-{
-// Get file extension
-std::string ext = y->path().extension().string();
-const char* extension = ext.c_str();
 
-// File path string
-std::string path = y->path().string();
+//
+// Created by refactor on 10/8/18.
+//
 
-// Replace the absolute data path with an empty string
-replace(path, data, "");
 
-// Get final cpk file
-const char* cpkFile = path.c_str();
+void main() {
+    // Get all files in /data
+    for (recursive_directory_iterator y(data), end; y != end; ++y) {
+        // If path is not a directory
+        if (is_directory(y->path())) {
+            continue;
+        }
+        // Get file extension
+        std::string ext = y->path().extension().string();
+        const char* extension = ext.c_str();
 
-// If the file is a cpk
-if (strcmp(extension, ".cpk") == 0) {
-// To prevent loading the game's cpk we will check if it's in the ignoredCpks array.
-if (!value_exists_in_array(cpkFile, ignoreCpks, IGNORE_CPKS_SIZE)) {
-// Replace the extension with an empty string
-replace(path, extension, "");
+        // File path string
+        std::string path = y->path().string();
 
-// Mount cpk
-if (!MountCpk(cpkFile)) {
-printf("File: %s could not be loaded.\n", cpkFile);
-}
-else printf("%s was successfully loaded.\n", cpkFile);
-}
-}
-}
+        // Replace the absolute data path with an empty string
+        replace(path, data, "");
+
+        // Get final cpk file
+        const char* cpkFile = path.c_str();
+
+        // If the file is a cpk
+        if (strcmp(extension, ".cpk") != 0) {
+            continue;
+        }
+        // To prevent loading the game's cpk we will check if it's in the ignoredCpks array.
+        if (value_exists_in_array(cpkFile, ignoreCpks, IGNORE_CPKS_SIZE)) {
+            continue;
+        }
+
+        // Replace the extension with an empty string
+        replace(path, extension, "");
+
+        // Mount cpk
+        if (!MountCpk(cpkFile)) {
+            printf("File: %s could not be loaded.\n", cpkFile);
+        } else printf("%s was successfully loaded.\n", cpkFile);
+
+
+    }
 }
